1dd2d12f11ca62ca5df65958f189ea359d6cff75
ZiJingCuan12 <cpy13951041292020@gmail.com>
2025-11-25 03:32:01 +0800

    🔧 Auto-update website config via EdgeOne Functions

--- src/websiteData.js
+++ src/websiteData.js
@@ -1,11 +1,11 @@
-// 网站数据 - 通过管理后台更新于 2025/7/8 21:45:45
+// 网站数据 - 通过管理后台更新于 2025/11/25 03:31:59
 
 // 站点配置
 export const siteConfig = {
-  "siteName": "BinNav",
-  "siteTitle": "BinNav - 导航",
+  "siteName": "MuFVps",
+  "siteTitle": "MuFVps - 导航",
   "siteLogo": "/assets/logo.png",
-  "siteDescription": "二进制导航页面，精选优质站点",
+  "siteDescription": "MuFVps导航页面，资源聚合",
   "icpRecord": "",
   "publicSecurityRecord": "",
   "publicSecurityRecordUrl": ""
@@ -79,18 +79,6 @@ export const websiteData = [
       "视频"
     ],
     "icon": "https://icon.nbvil.com/favicon?url=bilibili.com"
-  },
-  {
-    "id": 45,
-    "name": "知乎专栏",
-    "description": "二进制-知乎",
-    "url": "https://www.zhihu.com/people/mrblack-41-9",
-    "category": "author",
-    "tags": [
-      "知乎专栏",
-      "技术文章"
-    ],
-    "icon": "https://icon.nbvil.com/favicon?url=www.zhihu.com"
   },
   {
     "id": 1751617129171,
@@ -326,11 +314,44 @@ export const categories = [
   }
 ];
 
+// 搜索引擎配置
+export const searchEngines = [
+  { id: "bing", name: "必应", url: "https://www.bing.com/search?q=", color: "bg-blue-600" },
+  { id: "baidu", name: "百度", url: "https://www.baidu.com/s?wd=", color: "bg-red-600" },
+  { id: "google", name: "谷歌", url: "https://www.google.com/search?q=", color: "bg-green-600" },
+  { id: "internal", name: "站内搜索", url: "", color: "bg-purple-600" }
+];
+
+// 推荐内容配置
+export const recommendations = [
+  {
+    id: 1,
+    title: "阿里云",
+    description: "点击领取2000元限量云产品优惠券",
+    url: "https://aliyun.com",
+    type: "sponsor",
+    color: "from-blue-50 to-blue-100"
+  },
+  {
+    id: 2,
+    title: "设计资源",
+    description: "高质量设计素材网站推荐",
+    url: "#design_resources",
+    type: "internal",
+    color: "from-green-50 to-green-100"
+  }
+];
+
+// 热门标签
+export const popularTags = [
+  "设计工具", "免费素材", "UI设计", "前端开发", "图标库", "配色方案",
+  "设计灵感", "原型工具", "代码托管", "学习平台", "社区论坛", "创业资讯"
+];
 
 // 网站统计信息
 export const siteStats = {
   totalSites: websiteData.length,
   totalCategories: categories.length,
   totalTags: [...new Set(websiteData.flatMap(site => site.tags || []))].length,
-  lastUpdated: "2025-07-08"
+  lastUpdated: "2025-11-24"
 };
